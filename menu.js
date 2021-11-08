9581e9801853bdc1afa1f171530d700682e255a2
benjp009 <patin.benjamin@gmail.com>
2021-11-08 21:35:50 +0100

    fix linters

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 // a fonction that on click display the menu
 
 function myFunction() {
-  let x = document.getElementById('myLinks');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
+  let const = document.getElementById('myLinks');
+  if (const.style.display === 'block') {
+    const.style.display = 'none';
   } else {
-    x.style.display = 'block';
+    const.style.display = 'block';
   }
 }
