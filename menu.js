cdb1e9279c3605402f8a3fe42da01b5666939387
benjp009 <patin.benjamin@gmail.com>
2021-11-08 21:34:25 +0100

    Fix linters issue

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 // a fonction that on click display the menu
 
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  let x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
 }
