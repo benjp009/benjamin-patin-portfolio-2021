1ca49121a2d729f823943d9174986de050fad977
benjp009 <patin.benjamin@gmail.com>
2021-11-08 20:30:55 +0100

    Add js file and myFunction for the animation of the hamburger

---
+++ menu.js
@@ -0,0 +1,10 @@
+// a fonction that on click display the menu
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
