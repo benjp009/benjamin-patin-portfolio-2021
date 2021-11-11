d46f2295854339537b453d5a3befccb92429c7c5
benjp009 <patin.benjamin@gmail.com>
2021-11-11 22:04:42 +0100

    Trial and error for form validation requirements

---
+++ Javascript/form.js
@@ -0,0 +1,6 @@
+const usernameEl = document.querySelector('#name');
+const emailEl = document.querySelector('#mail');
+const confirmPasswordEl = document.querySelector('#msg');
+const form = document.querySelector('#contact-me-form');
+
+onkeyup="this.value = this.value.toLowerCase();"
