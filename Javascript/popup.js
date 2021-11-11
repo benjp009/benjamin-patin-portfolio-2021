cd8fa8d8307db446f0dc22126586e79277a4afe4
benjp009 <patin.benjamin@gmail.com>
2021-11-11 16:23:01 +0100

    Change " on linters

--- Javascript/popup.js
+++ Javascript/popup.js
@@ -1,26 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 // Get the modal
-let modal = document.getElementById('popup');
+const modal = document.getElementById('popup');
 
 // Get the button that opens the modal
-let btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName('close')[0];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = 'grid';
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+window.onclick = function (event) {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
 };
--- Javascript/portfolio.js
+++ Javascript/portfolio.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-let portfolio = [ {
+const portfolio = [{
   title: 'Tonic',
   clientInfo: {
-    clientName: "Canopy",
+    clientName: 'Canopy',
     clientProjectStyle: 'Back End Dev',
     clientProjectYear: '2015',
   },
   workSectionImage: {
-    src: "ressources/Work-1.png",
-    alt: "some picture about Benjamin Work"
+    src: 'ressources/Work-1.png',
+    alt: 'some picture about Benjamin Work',
   },
   projectInfoShort: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   projectInfoLong: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -17,13 +17,13 @@ let portfolio = [ {
 {
   title: 'Multi-Post Stories',
   clientInfo: {
-    clientName: "Gertrude",
+    clientName: 'Gertrude',
     clientProjectStyle: 'Full Stack Dev',
     clientProjectYear: '2018',
   },
   workSectionImage: {
-    src: "ressources/Work-2.png",
-    alt: "Picture reprenting the second work done by Benjamin"
+    src: 'ressources/Work-2.png',
+    alt: 'Picture reprenting the second work done by Benjamin',
   },
   projectInfoShort: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   projectInfoLong: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -32,13 +32,13 @@ let portfolio = [ {
 {
   title: 'Tonic',
   clientInfo: {
-    clientName: "Canopy",
+    clientName: 'Canopy',
     clientProjectStyle: 'Back End Dev',
     clientProjectYear: '2015',
   },
   workSectionImage: {
-    src: "ressources/Work-1.png",
-    alt: "some picture about Benjamin Work"
+    src: 'ressources/Work-1.png',
+    alt: 'some picture about Benjamin Work',
   },
   projectInfoShort: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   projectInfoLong: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -47,13 +47,13 @@ let portfolio = [ {
 {
   title: 'Multi-Post Stories',
   clientInfo: {
-    clientName: "Gertrude",
+    clientName: 'Gertrude',
     clientProjectStyle: 'Full Stack Dev',
     clientProjectYear: '2018',
   },
   workSectionImage: {
-    src: "ressources/Work-2.png",
-    alt: "Picture reprenting the second work done by Benjamin"
+    src: 'ressources/Work-2.png',
+    alt: 'Picture reprenting the second work done by Benjamin',
   },
   projectInfoShort: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   projectInfoLong: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -61,27 +61,27 @@ let portfolio = [ {
 }
 ]
 
-let projectDetails = ""
+let projectDetails = ''
 for ( let index in portfolio) {
   // let projectName = arr[index];
 projectDetails +=
-  `<section class="grid-item-1" id="grid">
-      <img class="work-section-image" src="${portfolio[index].workSectionImage.src}" alt="Picture reprenting the first work done by Benjamin">
-      <h4 id="popup-title">${portfolio[index].title}</h4>
-      <ul class="client-info">
-        <li class="client-name">${portfolio[index].clientInfo.clientName}</li>
-        <img class="client-section-counter" src="ressources/Counter.png" alt="Counter">
-        <li class="project-style">${portfolio[index].clientInfo.clientProjectStyle}</li>
-        <img class="client-section-counter" src="ressources/Counter.png" alt="Counter">
-        <li class="project-year">${portfolio[index].clientInfo.clientProjectYear}</li>
+  `<section class='grid-item-1' id='grid'>
+      <img class='work-section-image' src='${portfolio[index].workSectionImage.src}' alt='Picture reprenting the first work done by Benjamin'>
+      <h4 id='popup-title'>${portfolio[index].title}</h4>
+      <ul class='client-info'>
+        <li class='client-name'>${portfolio[index].clientInfo.clientName}</li>
+        <img class='client-section-counter' src='ressources/Counter.png' alt='Counter'>
+        <li class='project-style'>${portfolio[index].clientInfo.clientProjectStyle}</li>
+        <img class='client-section-counter' src='ressources/Counter.png' alt='Counter'>
+        <li class='project-year'>${portfolio[index].clientInfo.clientProjectYear}</li>
       </ul>
-      <p class="project-info">${portfolio[index].projectInfoShort}</p>
-      <ul class="language-used">
-        <li><div class="tags">${portfolio[index].languageUsed[0]}</div></li>
-        <li><div class="tags">${portfolio[index].languageUsed[1]}</div></li>
-        <li><div class="tags">${portfolio[index].languageUsed[2]}</div></li>
+      <p class='project-info'>${portfolio[index].projectInfoShort}</p>
+      <ul class='language-used'>
+        <li><div class='tags'>${portfolio[index].languageUsed[0]}</div></li>
+        <li><div class='tags'>${portfolio[index].languageUsed[1]}</div></li>
+        <li><div class='tags'>${portfolio[index].languageUsed[2]}</div></li>
       </ul>
-    <button type="button" class="button see-project-button"  id="${index}">See Project!</button>
+    <button type='button' class='button see-project-button'  id='btn'>See Project!</button>
   </section>`
 }
-document.getElementById("portfolio").innerHTML = projectDetails;
+document.getElementById('portfolio').innerHTML = projectDetails;
