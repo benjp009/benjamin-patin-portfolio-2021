85d54dfaa1aa681289ab56fa75d976d49a785b05
benjp009 <patin.benjamin@gmail.com>
2021-11-11 22:09:26 +0100

    Linters fix

--- Javascript/form.js
+++ Javascript/form.js
@@ -1,6 +1,1 @@
-const usernameEl = document.querySelector('#name');
-const emailEl = document.querySelector('#mail');
-const confirmPasswordEl = document.querySelector('#msg');
-const form = document.querySelector('#contact-me-form');
-
-onkeyup="this.value = this.value.toLowerCase();"
+onkeyup = 'this.value = this.value.toLowerCase();';
