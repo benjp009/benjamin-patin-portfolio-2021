984e388cdda723386f93f80eecef9cafe42fdfb8
benjp009 <patin.benjamin@gmail.com>
2021-11-11 16:30:47 +0100

    Punctuation linters

--- Javascript/portfolio2.js
+++ Javascript/portfolio2.js
@@ -1,11 +1,8 @@
 const seeProjectButton = document.querySelectorAll (
-  '.see-project-button'
+  '.see-project-button',
 );
 
 seeProjectButton.forEach((element) => {
   element.addEventListener('click', function(event) {
-    console.log(portfolio[element.id])
-  }
-)
-}
-)
+  };),
+});
