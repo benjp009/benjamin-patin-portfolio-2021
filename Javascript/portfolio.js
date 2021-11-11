7d3aa8f77d916fcc54e78e5bf2972182bc26e518
benjp009 <patin.benjamin@gmail.com>
2021-11-11 16:28:07 +0100

    Linters

--- Javascript/portfolio.js
+++ Javascript/portfolio.js
@@ -58,10 +58,10 @@ const portfolio = [{
   projectInfoShort: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   projectInfoLong: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   languageUsed: ['html', 'css', 'JavaScript'],
-}
+},
 ]
 
-let projectDetails = ''
+let projectDetails = '';
 for ( let index in portfolio) {
   // let projectName = arr[index];
 projectDetails +=
--- Javascript/portfolio2.js
+++ Javascript/portfolio2.js
@@ -1,8 +1,6 @@
-
-
-let seeProjectButton = document.querySelectorAll (
+const seeProjectButton = document.querySelectorAll (
   '.see-project-button'
-)
+);
 
 seeProjectButton.forEach((element) => {
   element.addEventListener('click', function(event) {
