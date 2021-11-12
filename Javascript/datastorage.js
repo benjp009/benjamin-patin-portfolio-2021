8bf4a94fe741c9b647a916a44cc0aaf1fbcb4f17
benjp009 <patin.benjamin@gmail.com>
2021-11-12 17:34:33 +0100

    Add constant for locsal storage

--- Javascript/datastorage.js
+++ Javascript/datastorage.js
@@ -1,3 +1,12 @@
 const name = document.getElementById('name');
 const mail = document.getElementById('mail');
 const msg = document.getElementById('msg');
+
+const store = () => {
+  const input = {
+    name: name.value,
+    mail: mail.value,
+    msg: msg.value,
+  };
+  localStorage.setItem('formInput', JSON.stringify(input));
+};
