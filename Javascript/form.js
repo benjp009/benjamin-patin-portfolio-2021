4c9c4c79de824ab8faa8d133c97bff2d47bbddf9
benjp009 <patin.benjamin@gmail.com>
2021-11-12 15:31:51 +0100

    Fix linter 3:1

--- Javascript/form.js
+++ Javascript/form.js
@@ -1,6 +1,5 @@
 onkeyup = 'this.value = this.value.toLowerCase();';
 
-
 const form = document.getElementById('contact-me-form');
 const email = document.getElementById('mail');
 const errorUpper = '&#9888; Your email is in uppercase. Please rewrite it in lowercase.';
